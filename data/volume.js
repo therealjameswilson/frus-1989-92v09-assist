2c112140d93910aba87fc900d305195f6e482aff
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 07:02:39 -0400

    Create FRUS assist page

---
+++ data/volume.js
@@ -0,0 +1,12 @@
+window.FRUS_VOLUME = {
+  "index": 15,
+  "doc_id": "frus1989-92v09",
+  "title": "1989\u20131992, Volume IX, Germany",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1989-92v09",
+  "repo_slug": "frus-1989-92v09-assist",
+  "title_ascii": "1989-1992, Volume IX, Germany",
+  "pages_url": "https://therealjameswilson.github.io/frus-1989-92v09-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1989-92v09-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=Germany&from=1989&to=1992&sort=relevance&perColl=25&perPage=50",
+  "companion": null
+};
